03796531a45542aab449729ed3ce883fbc52268f
Shivakumaraulli <113375129+Shivakumaraulli@users.noreply.github.com>
2023-07-04 22:26:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('node:http');
+const fs = require("node:fs")
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Home Page");
+    }
+    
+    else if( req.url === '/api'){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({
+            firstName: "Bruce",
+            lastName: "Wayne"
+        }));
+    
+    }
+    else{
+        res.writeHead(404);
+        res.end("Page Not Found");
+    }
+})
+server.listen(3000, () => {
+    console.log("Server running on Port 3000");
+})
